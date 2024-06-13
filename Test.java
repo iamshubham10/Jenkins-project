371bcd28ed1f0d719086d1e4db08681fa152aef8
Shubham Alchetti <alchettishubham@gmail.com>
2024-06-13 15:53:46 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.securityDemo.security.Utilities;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		System.out.println("jack");
+		System.out.println("Sparrow");
+		System.out.println(new Date());
+		
+	}
+
+}
