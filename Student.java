f11b1d58e88453a6c4468472614eaf2b09153160
Shubham Alchetti <alchettishubham@gmail.com>
2024-06-13 18:43:47 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,7 +1,7 @@
 public class Student {
     
     public Student(){
-        System.out.println("");
+        System.out.println("Addded new Student");
     }
     public void display(){
         System.out.println("Hi, I'm Student");
